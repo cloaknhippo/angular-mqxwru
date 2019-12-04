b852a143d9cdbf44ef6f47dab7c9fcfb1cd14cea
cloaknhippo <jtfelan1111@yahoo.com>
2019-12-03 18:34:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mqxwru
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mqxwru)
---
+++ src/dashboard/dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
+})
+export class AppComponent  {
+  name = 'AngularJoe';
+}
