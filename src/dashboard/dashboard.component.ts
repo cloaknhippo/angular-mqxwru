4f69b924eb987d670ad18969554268d87ed05df2
cloaknhippo <jtfelan1111@yahoo.com>
2019-12-03 20:53:36 -0600

    3 files modified

--- src/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+
 
 @Component({
   selector: 'my-app',
--- src/dashboard/menu-bar/menu-bar.component.ts
+++ src/dashboard/menu-bar/menu-bar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css']
+  
 })
 export class MenuBarComponent implements OnInit {
 
