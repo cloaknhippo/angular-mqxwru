ce1eef3d8b68df7bfd91a5a0c1c8d170bcbe11d4
cloaknhippo <jtfelan1111@yahoo.com>
2019-12-04 00:14:18 -0600

    5 files modified, 7 added

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './dashboard.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { ButtonBarComponent } from './button-bar/button-bar.component';
+import { SystemMessagesComponent } from './system-messages/system-messages.component';
+import { SatellitesComponent } from './satellites/satellites.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MenuBarComponent, ButtonBarComponent ],
+  declarations: [ AppComponent, MenuBarComponent, ButtonBarComponent, SystemMessagesComponent, SatellitesComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/dashboard/system-messages/system-messages.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'system-messages',
+  templateUrl: './system-messages.component.html',
+  styleUrls: ['./system-messages.component.css']
+})
+export class SystemMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  title = 'System Messages';
+
+}
