b43ec7da88395e2170815f2ab51b68153d238888
cloaknhippo <jtfelan1111@yahoo.com>
2019-12-03 21:06:15 -0600

    4 files modified, 3 added

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './dashboard.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { ButtonBarComponent } from './button-bar/button-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MenuBarComponent ],
+  declarations: [ AppComponent, MenuBarComponent, ButtonBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
